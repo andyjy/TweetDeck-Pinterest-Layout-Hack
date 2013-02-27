6c297a5f9c17a2c0068ac8e1ac41ac0b86daa016
AndyY <andy@apexa.co.uk>
2013-02-27 07:46:04 +0000

    added bookmarklet

---
+++ generate-bookmarklet.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+/**
+ * Handy script for generating the bookmarklet HTML file
+ */
+ob_start();
+$code = str_replace(array("\\\n", "\r", "\n"), '', preg_replace('|//.*\n|', '', file_get_contents('tweetdeck-pinterest.js')));
+?>
+<a href="<?php echo htmlspecialchars($code); ?>">TweetDeck Pinterest-style</a>
+<?php
+file_put_contents('bookmarklet.html', ob_get_clean());
